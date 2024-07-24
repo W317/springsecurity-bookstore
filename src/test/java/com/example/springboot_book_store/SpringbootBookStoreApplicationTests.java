145ce431d73319ba5e2e80d9cb43715c68320cab
W317 <liemvu202@gmail.com>
2024-07-24 14:20:21 +0700

    create project

---
+++ src/test/java/com/example/springboot_book_store/SpringbootBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_book_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
